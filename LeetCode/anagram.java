d74f42b4e5af5993747c4ed81e76c19733aa9bcb
Nitishprasadverma <nitishverma773964@gmail.com>
2024-11-14 23:55:27 +0530

    #leetcode #242

---
+++ LeetCode/anagram.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class anagram {
+    public static void main(String[] args) {
+        System.out.println(validAnagram("anagram", "nagaram"));
+    }
+    static boolean validAnagram(String s, String t){
+ Map<Character, Integer> count = new HashMap<>();
+        for(char x:s.toCharArray()){
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        for(char x:t.toCharArray()){
+          count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+
+        for(int value: count.values()){
+            if(value!= 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
