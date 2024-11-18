766ca47680b1d3109fd089478664324be68129cb
Nitishprasadverma <nitishverma773964@gmail.com>
2024-11-18 18:24:24 +0530

    #kunalLecture #backTracking NQueens,#StriverNqueen #leetCode #51,#52

---
+++ LeetCode/nqeenCount.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class nqeenCount {
+    public static void main(String[] args) {
+        System.out.println(solveNQueens(5));
+    }
+
+
+     public static int solveNQueens(int n){
+        char[][] board = new char[n][n];
+
+        for(int i   = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = '.';
+
+            }
+        }
+
+        List < List <String>> res = new ArrayList<>();
+        int leftRow[] = new int[n];
+        int upperDigonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+
+        solve(0, board,res,leftRow,lowerDiagonal,upperDigonal);
+        return res.size();
+    }
+
+    static void solve(int col, char[][] board, List<List<String>>res, int leftRow[], int lowerDiagonal[], int upperDigonal[]){
+        if(col == board.length){
+            res.add(construct(board));
+            return;
+        }
+        for(int row = 0; row < board.length; row++){
+            if(leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDigonal[board.length - 1 + col - row] == 0){
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDigonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDigonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                 lowerDiagonal[row + col ] = 0;
+                 upperDigonal[board.length - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    static List< String> construct(char[][] board){
+        List<String> res = new LinkedList<>();
+        for(int i = 0; i < board.length; i++ ){
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
--- LeetCode/sentencesimilirty.java
+++ LeetCode/sentencesimilirty.java
@@ -1,5 +1,9 @@
+import java.util.*;
 public class sentencesimilirty {
-    public static void main(String[] args) {
-        
+   public static void main(String[] args) {
+    
+   }
     }
-}
+
+   
+
