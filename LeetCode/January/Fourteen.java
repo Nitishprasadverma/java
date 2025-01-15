cc562c95a4cec38bd9977941332dd6a807bf4c99
Nitishprasadverma <nitishverma773964@gmail.com>
2025-01-15 23:56:47 +0530

    sdbjd

---
+++ LeetCode/January/Fourteen.java
@@ -0,0 +1,36 @@
+package January;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Fourteen {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumLength(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int count = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+            // char key = entry.getKey();
+            int value = entry.getValue();
+
+            while (value >= 3) {
+                count -= 2;
+            }
+        }
+
+        return count;
+    }
+
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+
+    }
+
+}
