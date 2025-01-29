c56dda1f1753247fa00cc5d775a46822435b58d7
Nitishprasadverma <nitishverma773964@gmail.com>
2025-01-29 22:50:56 +0530

    #iterative-traversal

--- StriverBinaryTree/Tree.java
+++ StriverBinaryTree/Tree.java
@@ -1,7 +1,10 @@
 package StriverBinaryTree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Tree {
     public class TreeNode {
@@ -52,32 +55,130 @@ public class Tree {
 
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> arr = new ArrayList<>();
- inOrder(arr,root);
- return arr;
+        inOrder(arr, root);
+        return arr;
 
     }
 
-public void inOrder(List<Integer>arr, TreeNode root){
-    if(root == null) return;
+    public void inOrder(List<Integer> arr, TreeNode root) {
+        if (root == null)
+            return;
 
-    inOrder( arr,root.left);
-    arr.add(root.val);
-    inOrder(arr,root.right);
+        inOrder(arr, root.left);
+        arr.add(root.val);
+        inOrder(arr, root.right);
+
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> arr = new ArrayList<>();
+        postOrder(arr, root);
+        return arr;
+    }
+
+    public void postOrder(List<Integer> arr, TreeNode root) {
+        if (root == null)
+            return;
+
+        postOrder(arr, root.left);
+        postOrder(arr, root.right);
+        arr.add(root.val);
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        List<List<Integer>> list = new LinkedList<List<Integer>>();
+        if (root == null)
+            return list;
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int levelNum = queue.size();
+
+            List<Integer> subList = new LinkedList<Integer>();
+
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null)
+                    queue.offer(queue.peek().left);
+                if (queue.peek().right != null)
+                    queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            list.add(subList);
+        }
+        return list;
+    }
+
+    // iterative preorder traversal
+    public List<Integer> iterativepPreorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        if (root == null)
+            return list;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            root = st.pop();
+            list.add(root.val);
+
+            if (root.right != null)
+                st.push(root.right);
+
+            if (root.left != null)
+                st.push(root.left);
+        }
+        return list;
+
+    }
+
+    // Iterative inorder traversal
+    public List<Integer> iterativeInorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while (true) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                if (stack.isEmpty()) {
+                    break;
+                }
+                node = stack.pop();
+                list.add(node.val);
+            }
+        }
+        return list;
+    }
+
+
+    //Iterative 
+    public List<Integer> IterativePostorderTraversal(TreeNode root){
+        Stack<TreeNode> st1 = new Stack<TreeNode>();
+        Stack<TreeNode> st2 = new Stack<TreeNode>();
+        List<Integer> list = new ArrayList<Integer>();
+
+        if(root == null) return list;
+
+        st1.push(root);
+        while (!st1.isEmpty()) {
+            root = st1.pop();
+            st2.add(root);
+
+            if(root.left != null) st1.add(root.left);
+            if(root.right != null) st1.push(root.right);
+        }
+
+        while (!st2.isEmpty()) {
+            list.add(st2.pop().val);
+        }
+
+        return list;
+    }
+
     
-}
-
-public List<Integer> postorderTraversal(TreeNode root) {
-    List<Integer> arr = new ArrayList<>();
-    postOrder(arr,root);
-    return arr;
-}
-
-public void postOrder(List<Integer> arr, TreeNode root){
-    if(root == null) return ;
-
-    postOrder(arr, root.left);
-    postOrder(arr,root.right);
-    arr.add(root.val);
-    
-}
 }
