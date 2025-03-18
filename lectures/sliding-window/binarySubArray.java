2e2596957105928d6eb8f8d5d2227b3038c0a419
Nitishprasadverma <nitishverma773964@gmail.com>
2025-03-18 09:39:10 +0530

    LeetCode sliding Window

---
+++ lectures/sliding-window/binarySubArray.java
@@ -0,0 +1,26 @@
+public class binarySubArray {
+
+    public static void main(String[] args) {
+        
+    }
+    public int numSubarraysWithSum(int[] nums, int goal){
+        int cnt1 = numSubarraysWithSumHelper(nums, goal);
+        int  cnt2 = numSubarraysWithSumHelper(nums, goal -1);
+        return cnt2 - cnt1;
+    }
+    public int numSubarraysWithSumHelper(int[] nums, int goal) {
+        if(goal < 0) return 0;
+        int l = 0, r =0, sum = 0, cnt  = 0;
+
+        while (r <= nums.length) {
+            sum += nums[r];
+            while (sum > goal) {
+                sum = sum - nums[l] ;
+                l++;
+            }
+            cnt =cnt + (r -l + 1);
+            r++;
+        }
+        return  cnt;
+    }
+}
