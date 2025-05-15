00153f331104f2996bc36dd34ffd0c256ed9f213
Nitishprasadverma <nitishverma773964@gmail.com>
2025-05-15 23:59:42 +0530

    LeetCode

---
+++ LeetCode/FifteenMay25.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+public abstract class FifteenMay25 {
+    
+}
+
+
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        int i = 0;
+        Set<Integer> set = new HashSet<>();
+
+        while(Math.pow(x,i) <= bound){
+int j = 0;
+            while((Math.pow(x,i) + Math.pow(y, j))<= bound){
+                int num =(int) (Math.pow(x,i) + Math.pow(y, j));
+                set.add(num );
+                if(y == 1) break;
+                j++;
+            }
+
+            if(x == 1) break;
+            i++;
+        }
+
+        List<Integer> ans = new ArrayList<>(set);
+        return ans;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution1 {
+    public int minDepth(TreeNode root) {
+       if(root == null) return 0;
+       if(root.left == null) return 1 + minDepth(root.right);
+
+       if(root.right == null) return 1+ minDepth(root.left);
+       
+
+       return 1 + Math.min(minDepth(root.left), minDepth(root.right));  
+    }
+}
