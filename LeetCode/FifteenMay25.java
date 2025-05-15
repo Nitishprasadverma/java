ad8b8672fa6ac2d3ea9c13c13de107da481fe90e
Nitishprasadverma <nitishverma773964@gmail.com>
2025-05-16 00:04:10 +0530

    LeetCode 15May

--- LeetCode/FifteenMay25.java
+++ LeetCode/FifteenMay25.java
@@ -4,7 +4,21 @@ public abstract class FifteenMay25 {
     
 }
 
+//LeetCode 2900+++++++++++++++++++++++++++++++++++++++++++
+class Solution1 {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> ans = new ArrayList<>();
 
+        for(int i = 0; i < groups.length; i++){
+            if(i == 0 || groups[i - 1] != groups[i]){
+                ans.add(words[i]);
+            }
+        }
+        return ans;
+    }
+}
+
+//LeetCode 970 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class Solution {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
         int i = 0;
@@ -28,29 +42,33 @@ int j = 0;
     }
 }
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution1 {
-    public int minDepth(TreeNode root) {
-       if(root == null) return 0;
-       if(root.left == null) return 1 + minDepth(root.right);
 
-       if(root.right == null) return 1+ minDepth(root.left);
+  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// LeetCode 111
+
+// /**
+//  * Definition for a binary tree node.
+//   public class TreeNode {
+//       int val;
+//     TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//           this.val = val;
+//           this.left = left;
+//          this.right = right;
+//       }
+//   }
+ 
+// class Solution1 {
+//     public int minDepth(TreeNode root) {
+//        if(root == null) return 0;
+//        if(root.left == null) return 1 + minDepth(root.right);
+
+//        if(root.right == null) return 1+ minDepth(root.left);
        
 
-       return 1 + Math.min(minDepth(root.left), minDepth(root.right));  
-    }
-}
+//        return 1 + Math.min(minDepth(root.left), minDepth(root.right));  
+//     }
+// }
