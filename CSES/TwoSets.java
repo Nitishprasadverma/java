f9b96566040d2c00111ebc16c2864aa10e54f910
Nitish prasad verma <123109128+Nitishprasadverma@users.noreply.github.com>
2026-09-10 07:18:09 +0530

    CSES code

--- CSES/TwoSets.java
+++ CSES/TwoSets.java
@@ -2,9 +2,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class TwoSets {
     public static void main(String[] args) throws Exception {
